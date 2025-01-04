801aeeec966a82039f2d623ca90d3cdc1055b9e8
guilherme <guiflr97@gmail.com>
2025-01-04 16:07:58 -0300

    config theme provider for styled components and type it

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from 'react'
 
-import { GlobalStyles } from '@/styles/global'
+import GlobalStyles from '@/styles/global'
 
 export function Providers({ children }: PropsWithChildren) {
   return (
