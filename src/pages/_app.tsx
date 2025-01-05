44e12bfbb91bdcf46b9657598f3f7de772363027
guilherme <guiflr97@gmail.com>
2025-01-05 15:30:49 -0300

    chrore: config _app to be able of using prefix $ on styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,26 @@ import GlobalStyles from '@/styles/global'
 import theme from '@/styles/theme'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { ThemeProvider } from 'styled-components'
+import { StyleSheetManager, ThemeProvider } from 'styled-components'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <StyleSheetManager enableVendorPrefixes>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>React Avançado - Boilerplate</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </StyleSheetManager>
   )
 }
 
