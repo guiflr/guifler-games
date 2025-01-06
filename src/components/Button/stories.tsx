a664f83544b99dc267881c9c3b6a6eb0b88719f1
guilherme <guiflr97@gmail.com>
2025-01-06 18:01:56 -0300

    feat: add Button component

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
+import Button from '.'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    icon: { table: { disable: true } }
+  }
+} as Meta
+
+export const Default: StoryObj = (args: any) => <Button {...args} />
+
+Default.args = {
+  children: 'Buy now'
+}
+
+export const withIcon: StoryObj = (args: any) => <Button {...args} />
+withIcon.args = {
+  size: 'small',
+  children: 'Buy now',
+  $icon: <AddShoppingCart />
+}
