9823492623bb04d797b3066bdd45eb59eb2c524e
guilherme <guiflr97@gmail.com>
2025-01-13 17:37:10 -0300

    feat: add Conteiner div

---
+++ src/components/Container/test.tsx
@@ -0,0 +1,24 @@
+import theme from '@/styles/theme'
+import { Container } from '.'
+import { renderWithTheme } from '@/utils/tests/helpers'
+
+describe('<Container />', () => {
+  const spy = jest.spyOn(console, 'warn').mockImplementation(() => {})
+
+  afterEach(() => {
+    spy.mockClear()
+  })
+
+  it('should render the heading', () => {
+    const { container } = renderWithTheme(
+      <Container>
+        <span>Won Games</span>
+      </Container>
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'max-width',
+      theme.grid.container
+    )
+  })
+})
