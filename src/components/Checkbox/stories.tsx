88872c2cfbf00a5562a95a065d660d254747409b
guilherme <guiflr97@gmail.com>
2025-01-16 14:12:41 -0300

    feat: Add text field component and update sotires

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -2,7 +2,7 @@ import { Meta } from '@storybook/react'
 import Checkbox, { CheckboxProps } from '.'
 
 export default {
-  title: 'Checkbox',
+  title: 'Form/Checkbox',
   component: Checkbox,
   parameters: {
     layout: 'fullscreen',
---
+++ src/components/TextField/styles.ts
@@ -0,0 +1,105 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+
+import { TextFieldProps } from '.'
+
+type IconPositionProps = Pick<TextFieldProps, 'iconPosition'>
+
+type WrapperProps = Pick<TextFieldProps, 'disabled'> & { error?: boolean }
+
+export const InputWrapper = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    background: ${theme.colors.lightGray};
+    border-radius: 0.2rem;
+    padding: 0 ${theme.spacings.xsmall};
+    border: 0.2rem solid;
+    border-color: ${theme.colors.lightGray};
+
+    &:focus-within {
+      box-shadow: 0 0 0.5rem ${theme.colors.primary};
+    }
+  `}
+`
+
+const inputForwardProp = (prop: string) => prop !== 'iconPosition'
+export const Input = styled.input.withConfig({
+  shouldForwardProp: inputForwardProp
+})<IconPositionProps>`
+  ${({ theme, iconPosition }) => css`
+    color: ${theme.colors.black};
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xxsmall} 0;
+    padding-${iconPosition}: ${theme.spacings.xsmall};
+    background: transparent;
+    border: 0;
+    outline: none;
+    width: 100%;
+  `}
+`
+
+export const Label = styled.label`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    cursor: pointer;
+  `}
+`
+
+const iconForwardProp = (prop: string) => prop !== 'iconPosition'
+export const Icon = styled.div.withConfig({
+  shouldForwardProp: iconForwardProp
+})<IconPositionProps>`
+  ${({ theme, iconPosition }) => css`
+    display: flex;
+    width: 2.2rem;
+    color: ${theme.colors.gray};
+    order: ${iconPosition === 'right' ? 1 : 0};
+
+    & > svg {
+      width: 100%;
+    }
+  `}
+`
+
+export const Error = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.colors.red};
+    font-size: ${theme.font.sizes.xsmall};
+  `}
+`
+
+const wrapperModifiers = {
+  error: (theme: DefaultTheme) => css`
+    ${InputWrapper} {
+      border-color: ${theme.colors.red};
+    }
+
+    ${Icon},
+    ${Label} {
+      color: ${theme.colors.red};
+    }
+  `,
+  disabled: (theme: DefaultTheme) => css`
+    ${Label},
+    ${Input},
+    ${Icon} {
+      cursor: not-allowed;
+      color: ${theme.colors.gray};
+
+      &::placeholder {
+        color: currentColor;
+      }
+    }
+  `
+}
+
+const wrapperForwardProp = (prop: string) => prop !== 'error'
+export const Wrapper = styled.div.withConfig({
+  shouldForwardProp: wrapperForwardProp
+})<WrapperProps>`
+  ${({ theme, error, disabled }) => css`
+    ${error && wrapperModifiers.error(theme)}
+    ${disabled && wrapperModifiers.disabled(theme)}
+  `}
+`
